bc93a7092a4f3c9d29e318f1434c72a0dbba18c6
YayCover9000 <yassuanf@googlemail.com>
2023-11-06 10:34:10 +0100

    fixed my Code with help

--- Testat1_Vorbereitung_Zinsrechnung.java
+++ Testat1_Vorbereitung_Zinsrechnung.java
@@ -1,22 +1,25 @@
-
+import java.util.Scanner;
 public class Testat1_Vorbereitung_Zinsrechnung {
 	public static void main(String [] args){
-        double K0 = 200;
-        double Kn = 0;
-        int n = 1;
-        double p = 5;
-        double i = p/100;
+        Scanner sc = new Scanner(System.in);
+        float AnfangsKapital = sc.nextFloat();
+        float EndKapital;
+        int Laufzeit = sc.nextInt();
+        float Zinssatz = sc.nextFloat();
+        float i;
+        float i2;
         
-        //Berechnung
-        
-        
-        if(p<10) {
-            Kn = K0 * Math.pow((1 + i), n);
-            System.out.println(Kn);
-
+        if(Zinssatz<10) {
+            i = Zinssatz/100.0f;
+            i2 = (i * Laufzeit + 1);
+            EndKapital = AnfangsKapital * i2;
+            //EndKapital = AnfangsKapital * Math.pow((Laufzeit + 1),i);
+            System.out.println(EndKapital);
         }else{
             System.out.println("Eingabefehler");
         }
+        sc.close();
+        
     }
 }
 
